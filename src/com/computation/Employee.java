89790510cb17b3d3ea341314e31113131f7bf022
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-09 21:32:23 +0530

    [kshitija] : part timeemployee and wage are added

--- src/com/computation/Employee.java
+++ src/com/computation/Employee.java
@@ -6,13 +6,23 @@ public class Employee {
 	System.out.println("Welcome to Employee Wage Computation");
 	
 	int IS_FULL_TIME=1;
-	double empCheck=Math.floor(Math.random()*10)%2;
+	int IS_PART_TIME=2;
+	int empRate=20;
+	int empHrs=0;
+	int empWage=0;
+	double empCheck=Math.floor(Math.random()*10)%3;
 	if (empCheck == IS_FULL_TIME) {
-		System.out.println("Employee is present");
+		
+		empHrs=8;
+		
+	}else if (empCheck == IS_PART_TIME){
+		empHrs=4;
 		
 	}else {
-		System.out.println("Employee is not present");
+		empHrs=0;
 	}
+	empWage=empHrs*empRate;
+	System.out.println("Employee Wage="+empWage);
 
 	}
 
