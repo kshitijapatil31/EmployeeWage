96867e1cac70869ba8a8657dda3d194717af4de7
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-09 14:43:06 +0530

    [kshitija] : Employee is present or not is checked

--- src/com/computation/Employee.java
+++ src/com/computation/Employee.java
@@ -3,7 +3,16 @@ package com.computation;
 public class Employee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Welcome to Employee Wage Computation");
+	
+	int IS_FULL_TIME=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if (empCheck == IS_FULL_TIME) {
+		System.out.println("Employee is present");
+		
+	}else {
+		System.out.println("Employee is not present");
+	}
 
 	}
 
