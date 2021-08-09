e548e26dcc0e9548b0618590e194eb9fc3eede0b
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-09 20:22:24 +0530

    [kshitija]: wage calculated

--- src/com/computation/Employee.java
+++ src/com/computation/Employee.java
@@ -6,13 +6,19 @@ public class Employee {
 	System.out.println("Welcome to Employee Wage Computation");
 	
 	int IS_FULL_TIME=1;
+	int empRate=20;
+	int empHrs=0;
+	int empWage=0;
 	double empCheck=Math.floor(Math.random()*10)%2;
 	if (empCheck == IS_FULL_TIME) {
-		System.out.println("Employee is present");
+		
+		empHrs=8;
 		
 	}else {
-		System.out.println("Employee is not present");
+		empHrs=0;
 	}
+	empWage=empHrs*empRate;
+	System.out.println("Employee Wage ="+empWage);
 
 	}
 
