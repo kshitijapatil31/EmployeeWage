c12171158509c3c4d3361a558a5b95faf21f3451
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-09 22:57:58 +0530

    [kshitija] : wages for a month calculated

--- src/com/computation/Employee.java
+++ src/com/computation/Employee.java
@@ -4,12 +4,17 @@ public class Employee {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int empRate=20;
+	public static final int WORKING_DAYS=2;
+	
+	
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation");
 	
 	
 	int empHrs=0;
 	int empWage=0;
+	int totalEmp=0;
+	for(int i=0;i<WORKING_DAYS;i++) {
 	double empCheck=Math.floor(Math.random()*10)%3;
 	switch((int)empCheck) {
 	case IS_FULL_TIME:
@@ -22,8 +27,11 @@ public class Employee {
 		empHrs =0;
 	}
 	empWage=empHrs*empRate;
+	totalEmp=totalEmp+empWage;
 	System.out.println("Employee Wage="+empWage);
 
+	}
+	System.out.println("Total Employee Wage="+totalEmp);
 	}
 
 }
